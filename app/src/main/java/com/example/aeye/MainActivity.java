c5c6156afc976a0324871b8a25138fb602f22d41
kevinlim17 <kevinlsh17@khu.ac.kr>
2022-07-13 00:12:32 +0900

    Implement Swipe Gesture Using ViewPaper

--- app/src/main/java/com/example/aeye/MainActivity.java
+++ app/src/main/java/com/example/aeye/MainActivity.java
@@ -2,111 +2,83 @@ package com.example.aeye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import androidx.viewpager.widget.ViewPager;
+import android.animation.ArgbEvaluator;
+
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     //Initialize Variables
     RelativeLayout relativeLayout;
-    TextView textView;
-    SwipeListener swipeListener;
+    TextView swipeCheckingText;
+
+    ViewPager viewPager;
+    Adapter adapter;
+    List<Mode> modes;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+    //Swiping Detector Class Instance
+    //SwipeListener swipeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Assign Variables
+
+        //Assign Variables for SwipeListener
         relativeLayout = findViewById(R.id.relative_layout);
-        textView = findViewById(R.id.text_view);
+        swipeCheckingText = findViewById(R.id.swipe_checking_text);
 
         //Initialize swipe listener
-        swipeListener = new SwipeListener(relativeLayout);
+        //swipeListener = new SwipeListener(relativeLayout, swipeCheckingText);
+        modes = new ArrayList<>();
+        modes.add(new Mode(R.drawable.drink_background2, "음료 구매하기"));
+        modes.add(new Mode(R.drawable.medicine_background, "의약품 구매하기"));
+
+        adapter = new Adapter(modes,this);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(100, 0, 100, 0);
+
+        Integer[] colors_temp = {
+                getResources().getColor(R.color.background_color1, null),
+                getResources().getColor(R.color.background_color2, null)
+        };
+
+        colors = colors_temp;
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if(position < (adapter.getCount() - 1) && position < (colors.length - 1)){
+                    viewPager.setBackgroundColor(
+                            (Integer) argbEvaluator.evaluate(
+                                    positionOffset,
+                                    colors[position],
+                                    colors[position + 1]
+                            )
+                    );
+                }
+                else{
+                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+                }
+            }
+            @Override
+            public void onPageSelected(int position) {}
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+
     }
-    private class SwipeListener implements View.OnTouchListener{
-        //Initialize Variables
-        GestureDetector gestureDetector;
 
-        //Create Constructor
-        SwipeListener(View view){
-            //Initialize Threshold Value
-            int threshold = 100;
-            int velocity_threshold = 100;
 
-            //Initialize simple gesture listener
-            GestureDetector.SimpleOnGestureListener listener =
-                    new GestureDetector.SimpleOnGestureListener(){
-                        @Override
-                        public boolean onDown(MotionEvent e) {
-                            //Pass true value
-                            return true;
-                        }
-                        @Override
-                        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                            //Get x and y difference
-                            float xDiff = e2.getX() - e1.getX();
-                            float yDiff = e2.getY() - e1.getY();
-                            try {
-                                //Check condition
-                                if(Math.abs(xDiff) > Math.abs(yDiff)){
-                                    //When x is greater than y
-                                    //Check condition
-                                    if(Math.abs(xDiff) > threshold
-                                            && Math.abs(velocityX) > velocity_threshold){
-                                        //When x difference is greater than threshold
-                                        //When x velocity is greater than velocity threshold
-                                        if(xDiff > 0){
-                                            //When swiped right
-                                            textView.setText("Swiped Right");
-                                        }
-                                        else{
-                                            //When swiped right
-                                            textView.setText("Swiped Left");
-                                        }
-                                        return true;
-                                    }
-                                }
-                                else{
-                                    //When y is greater than x
-                                    //Check condition
-                                    if(Math.abs(yDiff) > threshold
-                                            && Math.abs(velocityY) > velocity_threshold){
-                                        //When x difference is greater than threshold
-                                        //When x velocity is greater than velocity threshold
-                                        if(yDiff > 0){
-                                            //When swiped down
-                                            textView.setText("Swiped Down");
-                                        }
-                                        else{
-                                            //When swiped Up
-                                            textView.setText("Swiped Up");
-                                        }
-                                        return true;
-                                    }
-                                }
-                            }
-                            catch (Exception e){
-                                e.printStackTrace();
-                            }
-                            return false;
-                        }
-                    };
-            //Initialize gesture Detector
-            gestureDetector = new GestureDetector(listener);
-            //Set listener on View
-            view.setOnTouchListener(this);
-
-        }
-        @SuppressLint("ClickableViewAccessibility")
-        @Override
-        public boolean onTouch(View v, MotionEvent motionEvent) {
-            //Return gesture event
-            return gestureDetector.onTouchEvent(motionEvent);
-        }
-    }
 }
 
---
+++ app/src/main/java/com/example/aeye/Mode.java
@@ -0,0 +1,27 @@
+package com.example.aeye;
+
+public class Mode {
+    private int image;
+    private String title;
+
+    public Mode(int image, String title) {
+        this.image = image;
+        this.title = title;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
