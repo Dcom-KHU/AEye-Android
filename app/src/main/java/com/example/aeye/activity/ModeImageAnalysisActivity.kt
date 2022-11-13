379d1caff13e762e2dc5e96680d8294000d19178
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2022-11-13 21:23:41 +0900

    Move activities to new package and add new activity for image analysis

---
+++ app/src/main/java/com/example/aeye/activity/ModeImageAnalysisActivity.kt
@@ -0,0 +1,15 @@
+package com.example.aeye.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aeye.R
+
+
+class ModeImageAnalysisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_analysis)
+
+
+    }
+}
