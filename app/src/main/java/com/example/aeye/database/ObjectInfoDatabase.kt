e6a81a72288bdfbf76e31ca9b35c6a043925950f
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2022-11-20 15:19:38 +0900

    Add callback to initialize database at start

--- app/src/main/java/com/example/aeye/database/ObjectInfoDatabase.kt
+++ app/src/main/java/com/example/aeye/database/ObjectInfoDatabase.kt
@@ -4,23 +4,39 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [ObjectInfo::class], version = 1)
 abstract class ObjectInfoDatabase: RoomDatabase() {
     abstract fun objectInfoDao() : ObjectInfoDao
 
+    private class ObjectInfoDatabaseCallback(private val scope : CoroutineScope) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            instance?.let {
+                scope.launch {
+                    var objectInfoDao = it.objectInfoDao()
+
+                }
+            }
+        }
+    }
+
     companion object {
         private var instance: ObjectInfoDatabase? = null
 
         @Synchronized
-        fun getDataBase(context: Context) : ObjectInfoDatabase? {
+        fun getDataBase(context: Context, scope: CoroutineScope) : ObjectInfoDatabase? {
             if (instance == null){
-                synchronized(ObjectInfoDatabase::class){
+                synchronized(this){
                     instance = Room.databaseBuilder(
                         context.applicationContext,
                         ObjectInfoDatabase::class.java,
                         "object-info-database"
-                    )
+                    ).addCallback(ObjectInfoDatabaseCallback(scope))
                         .build()
                 }
             }
