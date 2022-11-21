b89f6b5f8177f7b84015d45947f7c8a87c0ee231
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2022-11-22 01:30:59 +0900

    Fix some mistakes

--- app/src/main/java/com/example/aeye/Mode.java
+++ app/src/main/java/com/example/aeye/Mode.java
@@ -1,3 +1,43 @@
-package com.example.aeye
+package com.example.aeye;
 
-class Mode(var image: Int, var icon: Int, var title: String, var title_color: Int)
+public class Mode {
+    private int image;
+    private int icon;
+    private String title;
+    private Integer title_color;
+
+    public Mode(int image, int icon, String title, Integer title_color) {
+        this.image = image;
+        this.icon = icon;
+        this.title = title;
+        this.title_color = title_color;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getIcon() { return icon; }
+
+    public void setIcon(int icon) { this.icon = icon; }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getTitle_color() {
+        return title_color;
+    }
+
+    public void setTitle_color(Integer title_color) {
+        this.title_color = title_color;
+    }
+}
